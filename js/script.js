ae69bf283e4a18c296f352f803c0f2c84e0e7eab
JoonaSar <35202620+JoonaSar@users.noreply.github.com>
2018-01-12 20:15:01 +0200

    New shit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+﻿var car = {
+	make: 'Audi',
+	type: 'A8',
+	color: 'Blue',
+	isTurnedOn: false,
+	numberOfWheels: 4,
+	seats: [
+	'seat 1', 
+	'seat 2', 
+	'seat 3'
+	],
+	turnOn: function () {
+		this.isTurnedOn = true;
+		alert( car.isTurnedOn);
+	},
+	
+	isCarOn: function () {
+		if (this.isTurnedOn == true) {
+			alert("Auto on päällä")
+		}
+		else {
+			alert("Auto ei ole päällä")
+		}
+		
+	},
+	
+	fly: function () {
+		alert('lennä nykäsen matti, lennä');
+	},
+	switchCar: function (isOn) {
+	if (isOn== true) {
+		this.isTurnedOn = true;
+		}
+	else {
+		this.isTurnedOn = false;
+		}
+	
+	}
+	};
+
+ doCoolStuff = function () {
+		var currentClassName = 
+			document.getElementById('cool').className;
+		if (currentClassName == 'cool'){
+			document.getElementById('cool').className = 'cool red';
+		}
+		else {
+			document.getElementById('cool').className = 'cool';
+		}
+		
+		
+	};
