8da13619dd51a206da10cefd7bb3d38aa0bf740c
JoonaSar <35202620+JoonaSar@users.noreply.github.com>
2018-01-12 19:16:02 +0200

    Initial commit

---
+++ muistiinpanot.js
@@ -0,0 +1,17 @@
+	var string = "lol";
+	var lista = ["lol", "ebin", "kys"];
+	var numero = 3;
+	var boo = true;
+	var richObject = { 	firstName: "Rich", 
+						lastName: "Armstrong",
+						yearsAlive: 192,
+						favColors: ['Red', "blue"]
+					}
+	
+	
+	var whatIsMyFavColor = function () {
+	
+	}
+	var doShit = function () {
+		console.log("do shit!!!");
+	}
